f2c3bb93adae44351f49d0308c00486353d7a5b8
Christopher Stamm <christopher.stamm@geoas.de>
2024-06-28 15:06:14 +0200

    1.23

--- Clarius.TransformOnBuild.MSBuild.Task/Clarius.TransformOnBuild.MSBuild.Task/TransformOnBuildTask.cs
+++ Clarius.TransformOnBuild.MSBuild.Task/Clarius.TransformOnBuild.MSBuild.Task/TransformOnBuildTask.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Build.Execution;
@@ -11,203 +10,195 @@ using Microsoft.Build.Framework;
 
 namespace Clarius.TransformOnBuild.MSBuild.Task
 {
-	// ReSharper disable once UnusedMember.Global
-	public class TransformOnBuildTask : Microsoft.Build.Utilities.Task
-	{
-		private ProjectInstance _projectInstance;
-		private Dictionary<string, string> _properties;
-		private string _programFiles;
-		private string _programFilesX86;
-		private string _commonProgramFiles;
-		const BindingFlags BindingFlags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public;
+    // ReSharper disable once UnusedMember.Global
+    public class TransformOnBuildTask : Microsoft.Build.Utilities.Task
+    {
+        private ProjectInstance _projectInstance;
+        private Dictionary<string, string> _properties;
+        private string _programFiles;
+        private string _programFilesX86;
+        private string _commonProgramFiles;
 
-		public override bool Execute()
-		{
-			_projectInstance = GetProjectInstance();
-			_properties = _projectInstance.Properties.ToDictionary(p => p.Name, p => p.EvaluatedValue);
+        const System.Reflection.BindingFlags BindingFlags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public;
 
-			var textTransformExePath = GetTextTransformExePath();
+        public override bool Execute()
+        {
+            _projectInstance = GetProjectInstance();
+            _properties = _projectInstance.Properties.ToDictionary(p => p.Name, p => p.EvaluatedValue);
 
-			var textTransform = _projectInstance.Items.Where(i =>
-				i.ItemType.IsOneOf(
-					values: new[] { "None", "Content" },
-					equalityComparer: StringComparer
-						.InvariantCultureIgnoreCase)
-				&& i.GetMetadataValue("Generator").IsOneOf(
-					values: new[] { "TextTemplatingFileGenerator", "TransformOnBuild" },
-					equalityComparer: StringComparer
-						.InvariantCultureIgnoreCase));
+            var textTransformExePath = GetTextTransformExePath();
 
-			var textTransformParameterItems = _projectInstance.Items
-				.Where(item => item.ItemType.Equals("TextTransformParameter", StringComparison.InvariantCultureIgnoreCase));
+            var textTransform = _projectInstance.Items.Where(i =>
+                i.ItemType.IsOneOf(
+                    values: new[] { "None", "Content" },
+                    equalityComparer: StringComparer
+                        .InvariantCultureIgnoreCase)
+                && i.GetMetadataValue("Generator").IsOneOf(
+                    values: new[] { "TextTemplatingFileGenerator", "TransformOnBuild" },
+                    equalityComparer: StringComparer
+                        .InvariantCultureIgnoreCase));
 
-			var textTransformParameters = string.Concat(
-				textTransformParameterItems.Select(item => $"-a \"!!{item.EvaluatedInclude}!{item.GetMetadataValue("Value")}\" "));
+            var textTransformParameterItems = _projectInstance.Items
+                .Where(item => item.ItemType.Equals("TextTransformParameter", StringComparison.InvariantCultureIgnoreCase));
 
-			foreach (var templateItem in textTransform)
-			{
-				var templatePath = templateItem.GetMetadataValue("FullPath");
-				var templateBackupPath = Path.GetTempFileName();
-				try
-				{
-					File.Copy(templatePath, templateBackupPath, overwrite: true);
+            var textTransformParameters = string.Concat(
+                textTransformParameterItems.Select(item => $"-a \"!!{item.EvaluatedInclude}!{item.GetMetadataValue("Value")}\" "));
 
-					RewriteTemplateFile(templatePath);
+            foreach (var templateItem in textTransform)
+            {
+                var templatePath = templateItem.GetMetadataValue("FullPath");
+                var templateBackupPath = Path.GetTempFileName();
+                try
+                {
+                    File.Copy(templatePath, templateBackupPath, overwrite: true);
 
-					var result = RunTransformTool(textTransformExePath, templatePath, textTransformParameters);
+                    RewriteTemplateFile(templatePath);
 
-					if (!result)
-						return false;
-				}
-				finally
-				{
-					File.Copy(templateBackupPath, templatePath, overwrite: true);
-					File.Delete(templateBackupPath);
-				}
-			}
+                    var result = RunTransformTool(textTransformExePath, templatePath, textTransformParameters);
 
-			return true;
-		}
+                    if (!result)
+                        return false;
+                }
+                finally
+                {
+                    File.Copy(templateBackupPath, templatePath, overwrite: true);
+                    File.Delete(templateBackupPath);
+                }
+            }
 
-		private void RewriteTemplateFile(string templatePath)
-		{
-			var encoding = GetCurrentEncoding(templatePath);
-			var template = File.ReadAllText(templatePath, encoding);
-			template = RewriteTemplateContent(template);
-			File.WriteAllText(templatePath, template, encoding);
-		}
+            return true;
+        }
 
-		private static Encoding GetCurrentEncoding(string templatePath)
-		{
-			using (var streamReader = new StreamReader(templatePath))
-			{
-				streamReader.ReadLine();
-				return streamReader.CurrentEncoding;
-			}
-		}
+        private void RewriteTemplateFile(string templatePath)
+        {
+            var encoding = GetCurrentEncoding(templatePath);
+            var template = File.ReadAllText(templatePath, encoding);
+            template = RewriteTemplateContent(template);
+            File.WriteAllText(templatePath, template, encoding);
+        }
 
-		private string RewriteTemplateContent(string template)
-		{
-			var result = Regex.Replace(template, @"(?im)(<#@\s*assembly\s+name\s*=\s*"".*?""|<#@\s*include\s+file\s*=\s*"".*?"")",
-				m => ExpandVariables(m.Value));
-			return result;
-		}
+        private static Encoding GetCurrentEncoding(string templatePath)
+        {
+            using (var streamReader = new StreamReader(templatePath))
+            {
+                streamReader.ReadLine();
+                return streamReader.CurrentEncoding;
+            }
+        }
 
-		private string ExpandVariables(string str)
-		{
-			var result = Environment.ExpandEnvironmentVariables(str);
-			result = Regex.Replace(result, @"\$\((?<PropertyName>.+?)\)", m => GetPropertyValue(m.Groups["PropertyName"].Value, throwIfNotFound: true));
-			return result;
-		}
+        private string RewriteTemplateContent(string template)
+        {
+            var result = Regex.Replace(template, @"(?im)(<#@\s*assembly\s+name\s*=\s*"".*?""|<#@\s*include\s+file\s*=\s*"".*?"")",
+                m => ExpandVariables(m.Value));
+            return result;
+        }
 
-		// ReSharper disable InconsistentNaming
-		private bool RunTransformTool(string textTransformExePath, string templatePath, string textTransformParameters)
-		// ReSharper restore InconsistentNaming
-		{
-			var process = new Process
-			{
-				StartInfo = new ProcessStartInfo
-				{
-					FileName = textTransformExePath,
-					Arguments = $"{textTransformParameters}\"{templatePath}\"",
-					RedirectStandardOutput = true,
-					RedirectStandardError = true,
-					CreateNoWindow = true,
-					UseShellExecute = false
-				}
-			};
-			process.ErrorDataReceived += (sender, args) =>
-			{
-				if (args.Data == null)
-					return;
-				Log.LogMessageFromText(args.Data, MessageImportance.Normal);
-			};
-			process.OutputDataReceived += (sender, args) =>
-			{
-				if (args.Data == null)
-					return;
-				Log.LogMessageFromText(args.Data, MessageImportance.Low);
-			};
-			process.Start();
-			process.BeginErrorReadLine();
-			process.BeginOutputReadLine();
-			process.WaitForExit();
-			return process.ExitCode == 0;
-		}
+        private string ExpandVariables(string str)
+        {
+            var result = Environment.ExpandEnvironmentVariables(str);
+            result = Regex.Replace(result, @"\$\((?<PropertyName>.+?)\)", m => GetPropertyValue(m.Groups["PropertyName"].Value, throwIfNotFound: true));
+            return result;
+        }
 
-		private string GetTextTransformExePath()
-		{
-			_commonProgramFiles = Environment.GetEnvironmentVariable("CommonProgramFiles(x86)");
-			if (string.IsNullOrEmpty(_commonProgramFiles))
-				_commonProgramFiles = GetPropertyValue("CommonProgramFiles");
+        // ReSharper disable InconsistentNaming
+        private bool RunTransformTool(string textTransformExePath, string templatePath, string textTransformParameters)
+        // ReSharper restore InconsistentNaming
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = textTransformExePath,
+                    Arguments = $"{textTransformParameters}\"{templatePath}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                }
+            };
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                if (args.Data == null)
+                    return;
+                Log.LogMessageFromText(args.Data, MessageImportance.Normal);
+            };
+            process.OutputDataReceived += (sender, args) =>
+            {
+                if (args.Data == null)
+                    return;
+                Log.LogMessageFromText(args.Data, MessageImportance.Low);
+            };
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            return process.ExitCode == 0;
+        }
 
-			_programFiles = Environment.GetEnvironmentVariable("ProgramW6432");
-            _programFilesX86 = Environment.GetEnvironmentVariable("ProgramFiles(x86)");			
+        private string GetTextTransformExePath()
+        {
+            _commonProgramFiles = Environment.GetEnvironmentVariable("CommonProgramFiles(x86)");
+            if (string.IsNullOrEmpty(_commonProgramFiles))
+                _commonProgramFiles = GetPropertyValue("CommonProgramFiles");
 
-			var textTransformPathCandidates = new[]
-			{
-				GetPropertyValue("TextTransformPath"),
-				$@"{_programFiles}\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\TextTransform.exe",
-				$@"{_programFiles}\Microsoft Visual Studio\2022\Professional\Common7\IDE\TextTransform.exe",
-				$@"{_programFiles}\Microsoft Visual Studio\2022\Community\Common7\IDE\TextTransform.exe",
-				$@"{_programFiles}\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2019\Professional\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2019\Community\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2017\Professional\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2017\Community\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\Preview\Professional\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\Preview\Enterprise\Common7\IDE\TextTransform.exe",
-				$@"{_programFilesX86}\Microsoft Visual Studio\Preview\Community\Common7\IDE\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\{GetPropertyValue("VisualStudioVersion")}\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\15.0\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\14.0\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\13.0\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\12.0\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\11.0\TextTransform.exe",
-				$@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\10.0\TextTransform.exe"
-			};
+            _programFiles = Environment.GetEnvironmentVariable("ProgramW6432");
+            _programFilesX86 = Environment.GetEnvironmentVariable("ProgramFiles(x86)");
 
-			foreach (var textTransformPathCandidate in textTransformPathCandidates)
-			{
-				if (!string.IsNullOrEmpty(textTransformPathCandidate) && File.Exists(textTransformPathCandidate))
-				{
-					return textTransformPathCandidate;
-				}
-			}
+            var textTransformPathCandidates = new[]
+            {
+                GetPropertyValue("TextTransformPath"),
+                $@"{_programFiles}\Microsoft Visual Studio\2022\Professional\Common7\IDE\TextTransform.exe",
+                $@"{_programFiles}\Microsoft Visual Studio\2022\Community\Common7\IDE\TextTransform.exe",
+                $@"{_programFiles}\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\TextTransform.exe",
+                $@"{_programFilesX86}\Microsoft Visual Studio\2019\Professional\Common7\IDE\TextTransform.exe",
+                $@"{_programFilesX86}\Microsoft Visual Studio\2019\Community\Common7\IDE\TextTransform.exe",
+                $@"{_programFilesX86}\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\{GetPropertyValue("VisualStudioVersion")}\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\15.0\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\14.0\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\13.0\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\12.0\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\11.0\TextTransform.exe",
+                $@"{_commonProgramFiles}\Microsoft Shared\TextTemplating\10.0\TextTransform.exe"
+            };
 
-			throw new Exception("Failed to find TextTransform.exe");
-		}
+            foreach (var textTransformPathCandidate in textTransformPathCandidates)
+            {
+                if (!string.IsNullOrEmpty(textTransformPathCandidate) && File.Exists(textTransformPathCandidate))
+                {
+                    return textTransformPathCandidate;
+                }
+            }
 
-		/// <summary>
-		/// Inspired by http://stackoverflow.com/questions/3043531/when-implementing-a-microsoft-build-utilities-task-how-to-i-get-access-to-the-va
-		/// </summary>
-		/// <returns></returns>
-		private ProjectInstance GetProjectInstance()
-		{
-			var buildEngineType = BuildEngine.GetType();
-			var targetBuilderCallbackField = buildEngineType.GetField("targetBuilderCallback", BindingFlags) ?? buildEngineType.GetField("_targetBuilderCallback", BindingFlags);
-			if (targetBuilderCallbackField == null)
-				throw new Exception("Could not extract targetBuilderCallback from " + buildEngineType.FullName);
-			var targetBuilderCallback = targetBuilderCallbackField.GetValue(BuildEngine);
-			var targetCallbackType = targetBuilderCallback.GetType();
-			var projectInstanceField = targetCallbackType.GetField("projectInstance", BindingFlags) ?? targetCallbackType.GetField("_projectInstance", BindingFlags);
-			if (projectInstanceField == null)
-				throw new Exception("Could not extract projectInstance from " + targetCallbackType.FullName);
-			return (ProjectInstance)projectInstanceField.GetValue(targetBuilderCallback);
-		}
+            throw new Exception("Failed to find TextTransform.exe");
+        }
 
-		// ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
-		private string GetPropertyValue(string propertyName, bool throwIfNotFound = false)
-		{
-			if (_properties.TryGetValue(propertyName, out var propertyValue))
-				return propertyValue;
-			if (throwIfNotFound)
-				throw new Exception($"Could not resolve property $({propertyName})");
-			return "";
-		}
-	}
+        /// <summary>
+        /// Inspired by http://stackoverflow.com/questions/3043531/when-implementing-a-microsoft-build-utilities-task-how-to-i-get-access-to-the-va
+        /// </summary>
+        /// <returns></returns>
+        private ProjectInstance GetProjectInstance()
+        {
+            var buildEngineType = BuildEngine.GetType();
+            var targetBuilderCallbackField = buildEngineType.GetField("targetBuilderCallback", BindingFlags) ?? buildEngineType.GetField("_targetBuilderCallback", BindingFlags);
+            if (targetBuilderCallbackField == null)
+                throw new Exception("Could not extract targetBuilderCallback from " + buildEngineType.FullName);
+            var targetBuilderCallback = targetBuilderCallbackField.GetValue(BuildEngine);
+            var targetCallbackType = targetBuilderCallback.GetType();
+            var projectInstanceField = targetCallbackType.GetField("projectInstance", BindingFlags) ?? targetCallbackType.GetField("_projectInstance", BindingFlags);
+            if (projectInstanceField == null)
+                throw new Exception("Could not extract projectInstance from " + targetCallbackType.FullName);
+            return (ProjectInstance)projectInstanceField.GetValue(targetBuilderCallback);
+        }
+
+        // ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
+        private string GetPropertyValue(string propertyName, bool throwIfNotFound = false)
+        {
+            if (_properties.TryGetValue(propertyName, out var propertyValue))
+                return propertyValue;
+            if (throwIfNotFound)
+                throw new Exception($"Could not resolve property $({propertyName})");
+            return "";
+        }
+    }
 }
